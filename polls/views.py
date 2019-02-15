9b130784efae18dffe1f67dee8c82236e019ecd5
Cilenti <1266308538@mil>
2019-02-15 11:30:42 -0500

    end of tutorial 3

--- polls/views.py
+++ polls/views.py
@@ -3,7 +3,7 @@ from django.template import loader
 from django.shortcuts import render, get_object_or_404, render
 from django.urls import reverse
 
-from .models import Question
+from .models import Question, Choice
 
 
 def index(request):
@@ -16,8 +16,8 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-    response = "You're looing at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
     
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
