b0313ed736eb6c3287990bb50dda959b2c2c6fa0
Cilenti <1266308538@mil>
2019-02-20 11:29:17 -0500

    end of tutorial 5

--- polls/views.py
+++ polls/views.py
@@ -2,12 +2,13 @@ from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import loader
 from django.shortcuts import render, get_object_or_404, render
 from django.urls import reverse
+from django.utils import timezone
 
 from .models import Question, Choice
 
 
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    latest_question_list = Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
 
